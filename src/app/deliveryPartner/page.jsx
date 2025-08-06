8b859a503b979cdee60017ab35f8c9c963841f58
shahbaz <shahbaznadeem654@gmail.com>
2025-08-06 16:45:09 +0500

    test user login 2

--- src/app/deliveryPartner/page.jsx
+++ src/app/deliveryPartner/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CommenLayout from '@components/layout/CommenLayout'
 import DeliveryPartnerSwitcher from '@components/deliveryPartner/DeliveryPartnerSwitcher'
 import { useDeliveryPartner } from '@/context/DeliveryPartnerContext';
